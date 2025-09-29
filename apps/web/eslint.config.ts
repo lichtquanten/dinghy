258675f8011b695f9c26473c2d2c23aac3862256
Aloe <magnetischen@gmail.com>
2025-09-29 09:58:54 -0700

    Reorganize tsconfig

--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -1,29 +1,29 @@
 // apps/web/eslint.config.ts
-import { defineConfig } from "eslint/config"
-import svelte from "eslint-plugin-svelte"
-import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config";
+import svelte from "eslint-plugin-svelte";
+import tseslint from "typescript-eslint";
 // @ts-ignore
-import rootConfig from "../../eslint.config"
+import rootConfig from "../../eslint.config";
 // @ts-ignore
-import svelteConfig from "./svelte.config.js"
+import svelteConfig from "./svelte.config.js";
 
 export default defineConfig([
-    // Inherit all configurations from root
-    ...rootConfig,
+  // Inherit all configurations from root
+  ...rootConfig,
 
-    // Add Svelte plugin's recommended flat config
-    ...svelte.configs["flat/recommended"],
+  // Add Svelte plugin's recommended flat config
+  ...svelte.configs["flat/recommended"],
 
-    // Configure parser options for Svelte files
-    {
-        files: ["**/*.svelte"],
-        languageOptions: {
-            parserOptions: {
-                parser: tseslint.parser,
-                extraFileExtensions: [".svelte"],
-                svelteConfig,
-            },
-            tsconfigRootDir: import.meta.dirname,
-        },
+  // Configure parser options for Svelte files
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".svelte"],
+        svelteConfig,
+      },
+      tsconfigRootDir: import.meta.dirname,
     },
-])
+  },
+]);
--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,13 +1,13 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
-    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
-    compilerOptions: {
-        experimental: {
-            async: true,
-        },
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    experimental: {
+      async: true,
     },
-}
+  },
+};
